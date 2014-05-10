243413f26de04044f969f216cea345bca4a39c8f
Takashi Yoshida <takashi@takashiyoshida.org>
2014-05-10 18:51:48 +0800

    Bump LaunchBar version to 5.6.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ class launchbar {
   package { 'Launchbar':
     ensure   => installed,
     provider => 'appdmg_eula',
-    source   => 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.6.2.dmg',
+    source   => 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.6.4.dmg',
   }
 }
